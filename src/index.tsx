5ad4604b03325ba3eca77e62f5f4859d4a909825
Maple-ning <517356766@qq.com>
2023-08-15 20:41:22 +0800

    完善登录页面

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
+import "@/assets/styles/global.scss";
 
 import { Provider } from "react-redux";
 import store from "./redux";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLDivElement
+);
 
 root.render(
   <React.StrictMode>
@@ -16,4 +19,4 @@ root.render(
       </Router>
     </Provider>
   </React.StrictMode>
-)
+);
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Checkbox, Form, Input } from "antd";
 import { setLocalStorage } from "@/utils/publicFn";
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import styles from "./index.module.scss";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import "./index.scss";
 
 type FieldType = {
   username?: string;
@@ -16,62 +16,76 @@ const Login: React.FC = () => {
   const onFinish = (values: any) => {
     setLocalStorage("learn-token", "test");
     setTimeout(() => {
-      navigate('/test')
+      navigate("/test");
     }, 200);
-    console.log('Success:', values);
+    console.log("Success:", values);
   };
 
   const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
+    console.log("Failed:", errorInfo);
   };
   return (
-    <div className={styles.loginView}>
-      <Form
-        name="basic"
-        size="large"
-        labelCol={{ span: 0 }}
-        wrapperCol={{ span: 24 }}
-        style={{
-          minWidth: 360,
-          maxWidth: '75vw',
-        }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item<FieldType>
-          name="username"
-          rules={[{ required: true, message: '请输入用户名' }]}
-        >
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
-        </Form.Item>
+    <div className="login-background">
+      <div className="login-container">
+        <div className="login-top">Sign up</div>
+        <div className="login-form">
+          <Form
+            name="basic"
+            size="large"
+            labelCol={{ span: 0 }}
+            wrapperCol={{ span: 24 }}
+            style={{
+              minWidth: 280,
+              maxWidth: "75vw",
+            }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item<FieldType>
+              name="username"
+              rules={[{ required: true, message: "请输入用户名" }]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="用户名"
+              />
+            </Form.Item>
 
-        <Form.Item<FieldType>
-          name="password"
-          rules={[{ required: true, message: '请输入密码' }]}
-        >
-          <Input.Password prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="密码" />
-        </Form.Item>
+            <Form.Item<FieldType>
+              name="password"
+              rules={[{ required: true, message: "请输入密码" }]}
+            >
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="密码"
+              />
+            </Form.Item>
 
-        <Form.Item<FieldType>
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{ offset: 0, span: 16 }}
-        >
-          <Checkbox>自动登录</Checkbox>
-        </Form.Item>
+            <Form.Item<FieldType>
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{ offset: 0, span: 16 }}
+            >
+              <Checkbox>自动登录</Checkbox>
+            </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 0, span: 24 }}>
-          <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
-            登录
-          </Button>
-        </Form.Item>
-      </Form>
+            <Form.Item wrapperCol={{ offset: 0, span: 24 }}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ width: "100%" }}
+              >
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
